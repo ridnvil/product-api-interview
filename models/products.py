d75ee861296ad367f1198ab1fb5d18a1f7388e3d
Ridwan <rid.nvil17@gmail.com>
2025-09-24 22:38:30 +0700

    fixing price type

--- models/products.py
+++ models/products.py
@@ -6,5 +6,5 @@ class Product(db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
     quantity = db.Column(db.Integer, default=0)
